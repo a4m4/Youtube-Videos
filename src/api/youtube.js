773f6c6c5deb7e4ca48ceb1d438f822525a999d8
Ali Mehdi <salimehdi144@gmail.com>
2019-05-18 02:19:04 +0500

    Update youtube.js

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,1 +1,13 @@
-const KEY = 'AIzaSyB7LL_EQx-pmDouUSgbhNoP_MLVLSYbfyI';
+import axios from 'axios';
+
+const KEY = 'AIzaSyCHNY5s1lIS-Pvr0LvguPAEvNrTzbgfYpo';
+
+
+export default axios.create({
+    baseUR: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
